b1a762a4e8635618afa918c68d7c17bdd0e126a3
Arusantimo_macbook <ziziana0507@naver.com>
2017-01-22 11:54:30 +0900

    wip

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "jasmine": true
+    },
+    "extends": ["eslint:recommended", "plugin:jasmine/recommended"],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        'jasmine'
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
