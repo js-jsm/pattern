8bc57fb130824a98b69989406e62cb993e5d6ab0
hwangtan <hwangtan@naver.com>
2017-02-12 11:05:35 +0900

    local

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,7 @@ module.exports = {
     ],
     "rules": {
         "indent": [
-            "error",
+            "error", // 0:pass 1:warning 2: error
             4
         ],
         "linebreak-style": [
@@ -27,7 +27,7 @@ module.exports = {
             "unix"
         ],
         "quotes": [
-            "error",
+            "error", // 이걸 무시하는 옵션으로 변경해야 템플릿 리터럴이 필터링되지않음
             "single"
         ],
         "semi": [
